f42a6e0fd50589f60d1efb84b3911dacd7030371
jujirohamma6-code <jujirohamma6@gmail.com>
2026-06-16 08:37:49 -0600

    feat: init project structure with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to JobMatch IA!</h1>
+    </div>
+  );
+};
+
+export default App;
